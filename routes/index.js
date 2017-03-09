6cec8ccce9ed79000a4e9fe95e625706bc8cb176
Cameron Sima <cjsima@gmail.com>
2017-03-09 17:45:34 -0500

    log host

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var hostname = req.headers.host.split(':')[0]
+  console.log(req.headers.host)
   res.render('index', { title: 'Party HQ', main: true });
 });
 
